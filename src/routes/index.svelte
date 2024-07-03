544d9364a141b310a7dd98afbaf1284fa871c883
shahul01 <shahulhammeed01@gmail.com>
2024-07-03 18:34:56 +0530

    refactor: indentation converted to spaces

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,72 +1,72 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+    import { onMount } from 'svelte';
 
 
   const loadingImg = 'https://i.stack.imgur.com/h6viz.gif';
   const fetchURL = 'https://dog.ceo/api/breeds/image/random';
-	let data = '';
-	let breedName = 'Loading';
-	let URLList = [];
-	let currImgPos = 0;
+    let data = '';
+    let breedName = 'Loading';
+    let URLList = [];
+    let currImgPos = 0;
   $: currURL = loadingImg;
 
-	onMount( () => {
-		// to prevent unnecessarily fetching when writing code...
-		setTimeout(() => {
-			fetchImage('DontIncrementImgPos');
-		}, 1000);
+    onMount( () => {
+        // to prevent unnecessarily fetching when writing code...
+        setTimeout(() => {
+            fetchImage('DontIncrementImgPos');
+        }, 1000);
 
-	});
+    });
 
-	function logSimpleURL() {
-		let simpleURLList = URLList;
-		simpleURLList = simpleURLList.map(el => {
+    function logSimpleURL() {
+        let simpleURLList = URLList;
+        simpleURLList = simpleURLList.map(el => {
       return el.split("/")[4];
-		});
-		console.log('simpleURLList', simpleURLList);
+        });
+        console.log('simpleURLList', simpleURLList);
 
-	};
+    };
 
-	async function fetchImage(isIncrementImgPos) {
+    async function fetchImage(isIncrementImgPos) {
     currURL = loadingImg;
-		const response = await fetch(fetchURL);
+        const response = await fetch(fetchURL);
     data = await response.json();
-		setFetchedData(isIncrementImgPos);
-	};
+        setFetchedData(isIncrementImgPos);
+    };
 
   function updateCurrURL() {
     currURL = URLList[currImgPos];
   };
 
-	function setFetchedData(isIncrementImgPos) {
-		URLList = [...URLList, data.message];
+    function setFetchedData(isIncrementImgPos) {
+        URLList = [...URLList, data.message];
     if (isIncrementImgPos === 'incrementImgPos') currImgPos+=1;
     updateCurrURL();
     logSimpleURL();
-		getBreedName();
-	};
+        getBreedName();
+    };
 
-	function getBreedName() {
+    function getBreedName() {
     updateCurrURL();
-		breedName = currURL?.split("/")[4];
+        breedName = currURL?.split("/")[4];
     // if (typeof(breedName) === 'undefined') return breedName = 'Loading';
-		breedName = breedName?.split("-")?.join(' ');
-		breedName = breedName?.charAt(0)?.toUpperCase() + breedName?.slice(1);
-	};
+        breedName = breedName?.split("-")?.join(' ');
+        breedName = breedName?.charAt(0)?.toUpperCase() + breedName?.slice(1);
+    };
 
-	function handlePrev() {
+    function handlePrev() {
     currImgPos-=1;
-		getBreedName();
-	};
+        getBreedName();
+    };
 
-	async function handleNext() {
+    async function handleNext() {
     if ( currURL === URLList[URLList.length - 1]) {
       await fetchImage('incrementImgPos');
-		} else {
+        } else {
       currImgPos+=1;
       getBreedName();
     };
-	};
+    };
 
 </script>
 
@@ -91,36 +91,36 @@
 
 
 <style lang="postcss">
-	.container {
-		display: flex;
-		justify-content: center;
-		flex-direction: column;
-	}
-	.breed-name{
-		text-align: center;
-	}
-	#myImg {
-		min-height: 40vh;
-		max-height: 80vh;
-		min-width: 40vw;
-		max-width: 97vw;
-	}
-	.btn-container {
-		position: absolute;
-		top: 94px;
-		display: flex;
-		justify-content: space-between;
-		align-items: flex-end;
-		width: 97vw;
-	}
-	button {
-		cursor: pointer;
-		user-select: none;
-	}
-	.arrow {
-		height: 80vh;
-		width: 40px;
-		background: rgba(255,255,255,0.6);
-		border-radius: 0px;
-	}
+    .container {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .breed-name{
+        text-align: center;
+    }
+    #myImg {
+        min-height: 40vh;
+        max-height: 80vh;
+        min-width: 40vw;
+        max-width: 97vw;
+    }
+    .btn-container {
+        position: absolute;
+        top: 94px;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        width: 97vw;
+    }
+    button {
+        cursor: pointer;
+        user-select: none;
+    }
+    .arrow {
+        height: 80vh;
+        width: 40px;
+        background: rgba(255,255,255,0.6);
+        border-radius: 0px;
+    }
 </style>
